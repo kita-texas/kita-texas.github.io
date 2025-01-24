023b82b531e39e416ed1db232d3a6054baae7018
Ueno_dreamyducks <taku0katy@duck.com>
2025-01-24 17:19:02 -0600

    login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,11 +3,16 @@ import './Login.css';
 
 function Login() {
     return(
-        <div className='login align-center'>
-            <h1 className='flex-text-align-left horizontal-padding-8'>Log in</h1>
-
-            <div style={{width: "300px", height: "200px"}} className='primary-container align-center'>
-                <h2>Log In</h2>
+        <div className='login'>
+            <div className='login-container'>
+                <h1 className='text-align-left' style={{margin: "0 0 16px"}}>Log in</h1>
+                <form action='/#/' method='get'>
+                    <input id='username' type='text' placeholder='Username' className='login-input'></input>
+                    <input id='password' type='password' placeholder='Password' className='login-input'></input>
+                    <button type='submit' className='login-button'>Log in</button>
+                </form>  
+                <hr />
+                <a href='/#' style={{textDecoration: "none", color: "#0000FF"}}>Sign up from this link</a>
             </div>
         </div>
     )
